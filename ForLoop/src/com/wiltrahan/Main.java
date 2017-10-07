131eb151f6b190add5116f6fdce36ee2b4bc7f46
wiltrahan <twilorip@gmail.com>
2017-10-07 19:11:51 -0400

    for loop

--- ForLoop/src/com/wiltrahan/Main.java
+++ ForLoop/src/com/wiltrahan/Main.java
@@ -8,6 +8,45 @@ public class Main {
         for(int i = 0; i < 5; i++) {
             System.out.println("Loop " + i + " hello!");
         }
+
+        for(int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.00, i)));
+        }
+
+        System.out.println("*******");
+
+        for(int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.00, i)));
+        }
+        System.out.println("*******");
+
+        int count = 0;
+        for (int i = 10; i < 50; i++){
+            if(isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+
+
+    public static boolean isPrime(int n) {
+        if(n == 1) {
+            return false;
+        }
+        for(int i = 2; i < n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
