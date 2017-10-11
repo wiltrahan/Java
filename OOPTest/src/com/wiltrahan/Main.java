4d64f2f280598f22a5d557deaadd2d4e6f15b8c2
wiltrahan <twilorip@gmail.com>
2017-10-11 18:25:27 -0400

    Lecture51 OOPTest

--- OOPTest/src/com/wiltrahan/Main.java
+++ OOPTest/src/com/wiltrahan/Main.java
@@ -9,5 +9,13 @@ public class Main {
 	    hamburger.addHamburgerAddition2("Lettuce", 0.25);
 	    hamburger.addHamburgerAddition3("Cheese", 1.25);
         System.out.println("Total burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total healthy burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.itemizeHamburger();
     }
 }
