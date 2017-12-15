72bbace60d91f611c113695fd8c2fb9db10b64f7
wiltrahan <twilorip@gmail.com>
2017-12-15 14:42:23 -0500

    refactor arrays

--- Arrays/src/com/wiltrahan/Main.java
+++ Arrays/src/com/wiltrahan/Main.java
@@ -8,25 +8,26 @@ public class Main {
 
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
-        for (int i = 0; i < myIntegers.length; i++) {
+        for(int i = 0; i < myIntegers.length; i++) {
             System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
-        System.out.println("The average is " + getAverage(myIntegers));
+
+        System.out.println("Average is " + getAvg(myIntegers));
     }
 
     public static int[] getIntegers(int number) {
         System.out.println("Enter " + number + " integer values.\r");
         int[] values = new int[number];
 
-        for(int i = 0; i < values.length; i++) {
+        for (int i = 0; i < values.length; i++) {
             values[i] = scanner.nextInt();
         }
         return values;
     }
 
-    public static double getAverage(int[] array) {
+    public static double getAvg(int[] array) {
         int sum = 0;
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         return (double) sum / (double)array.length;
