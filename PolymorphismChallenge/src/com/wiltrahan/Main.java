7d4785349a9acfdf62a37421bbb117094330c336
wiltrahan <twilorip@gmail.com>
2017-12-15 13:31:28 -0500

    PolyExerciseRefactored

--- PolymorphismChallenge/src/com/wiltrahan/Main.java
+++ PolymorphismChallenge/src/com/wiltrahan/Main.java
@@ -1,59 +1,7 @@
 package com.wiltrahan;
 
-class Car {
-    private boolean engine;
-    private int cylinders;
-    private String name;
-    private int wheels;
 
-    public Car(int cylinders, String name) {
-        this.cylinders = cylinders;
-        this.name = name;
-        this.wheels = 4;
-        this.engine = true;
-    }
 
-    public int getCylinders() {
-        return cylinders;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String startEngine() {
-        return "Car -> startEngine()";
-    }
-
-    public String accelerate() {
-        return "Car -> accelerate()";
-    }
-
-    public String brake() {
-        return "Car -> brake()";
-    }
-}
-
-class Tesla extends Car {
-    public Tesla(int cylinders, String name) {
-        super(cylinders, name);
-    }
-
-    @Override
-    public String startEngine() {
-        return "Tesla -> startEngine()";
-    }
-
-    @Override
-    public String accelerate() {
-        return "Tesla -> accelerate()";
-    }
-
-    @Override
-    public String brake() {
-        return "Tesla -> brake()";
-    }
-}
 
 public class Main {
 
