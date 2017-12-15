7dde50b6c892467affd14c4df3f216e4e51388b5
wiltrahan <twilorip@gmail.com>
2017-12-14 20:03:13 -0500

    begin polymorphism lesson

---
+++ Polymorphism2/src/com/wiltrahan/Main.java
@@ -0,0 +1,71 @@
+package com.wiltrahan;
+
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze.";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe.";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable(String name) {
+        super("Forgetable");
+    }
+    // no plot method
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
