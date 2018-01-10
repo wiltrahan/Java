8077623ededfd0e6efc006e24aa5f4882010580c
wiltrahan <twilorip@gmail.com>
2018-01-10 16:59:36 -0500

    Re-Factor Repo

--- 8_Composition_Encapsulation_Polymorphism/Composition/src/com/wiltrahan/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package com.wiltrahan;
-
-public class Vehicle {
-    private String name;
-
-    public Vehicle(String name) {
-        this.name = name;
-    }
-}
--- 8_Composition_Encapsulation_Polymorphism/Encapsulation/src/com/wiltrahan/Main.java
+++
@@ -1,9 +0,0 @@
-package com.wiltrahan;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    EnhancedPlayer player = new EnhancedPlayer("Wil", 77,"Rocket Launcher");
-        System.out.println("Initial health is: " + player.getHealth());
-    }
-}
