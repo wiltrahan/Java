cded24c66bd64d565d400937f5b6da3938d1b61f
wiltrahan <twilorip@gmail.com>
2018-01-10 18:17:35 -0500

    ClassesChallenge

---
+++ 07_Classes_Constructors_Inheritance/ClassesChallenge/src/com/wiltrahan/Main.java
@@ -0,0 +1,16 @@
+package com.wiltrahan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    BankAccount wilAccount = new BankAccount();
+	    wilAccount.setCustomerName("Wil Trahan");
+	    wilAccount.setBalance(143.90);
+        wilAccount.setAccountNumber(8675309);
+        wilAccount.setEmail("ohHai@us.gov");
+        wilAccount.setPhoneNumber("401-444-4444");
+        System.out.println("Account for " + wilAccount.getCustomerName() + " has a balance of " + wilAccount.getBalance());
+        wilAccount.withdrawal(19.00);
+        wilAccount.withdrawal(200.00);
+    }
+}
