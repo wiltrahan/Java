ff79c7b25cf2d1cdc58d91abcd7f78b9f46adf2d
wiltrahan <twilorip@gmail.com>
2018-01-30 19:52:46 -0500

    sets challenge 2

---
+++ 13_Collections/SetAndHashSet/src/com/wiltrahan/DwarfPlanet.java
@@ -0,0 +1,10 @@
+package com.wiltrahan;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+
+
+}
--- 13_Collections/SetAndHashSet/src/com/wiltrahan/HeavenlyBody.java
+++ 13_Collections/SetAndHashSet/src/com/wiltrahan/HeavenlyBody.java
@@ -3,7 +3,7 @@ package com.wiltrahan;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class HeavenlyBody {
+public abstract class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
